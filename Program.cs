dd0401ed408a4fd184c3799f34a69e2e07d80e52
NikolaDodnikov <ndodnikov@gmail.com>
2025-10-05 14:50:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConcatenateData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName=Console.ReadLine();
+            string lastName=Console.ReadLine();
+            int age=int.Parse(Console.ReadLine());
+            string town=Console.ReadLine();
+            if (age > 1)
+            {
+                Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+            }
+            else if( age < 1)
+            {
+                Console.WriteLine("Invalid number of age! Please try again.");
+            }
+            else
+            {
+                Console.WriteLine($"You are {firstName} {lastName}, a {age}-year old person from {town}.");
+            }
+        }
+    }
+}
